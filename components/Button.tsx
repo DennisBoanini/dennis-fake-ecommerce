4c13b64e67a379b19b3404575a4f3f52af5d75b7
Dennis A. Boanini <dennis.boanini01@gmail.com>
2024-01-15 09:53:51 +0100

    Add remove product from cart feature

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,13 +3,14 @@ import Spinner from '@/components/Spinner';
 type Props = {
 	buttonText: string;
 	isLoading?: boolean;
+	style?: 'danger';
 	onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button(props: Props) {
 	return (
 		<button
-			className={`border-[1px] border-primary-black rounded-md w-full py-3 px-5 bg-primary-black text-white uppercase ${
+			className={`border-[1px] ${props.style === 'danger' ? 'border-red-500 bg-red-500' : 'border-primary-black bg-primary-black'} rounded-md w-full py-3 px-5 text-white uppercase ${
 				props.isLoading || props.disabled ? 'hover:cursor-not-allowed opacity-10' : 'hover:cursor-pointer hover:bg-white hover:text-primary-black'
 			}`}
 			onClick={props.onClick}
--- components/CartProduct.tsx
+++ components/CartProduct.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image';
 import { CartProduct } from '@/models/CartProduct';
 import QuantitySelector from '@/components/QuantitySelector';
 import { toCurrency } from '@/utils/misc';
-import { updateQuantityProductInCart } from '@/app/actions';
+import { removeProductFromCart, updateQuantityProductInCart } from '@/app/actions';
+import Button from '@/components/Button';
 
 type Props = {
 	cartProduct: CartProduct;
@@ -22,15 +23,22 @@ export default function CartProduct(props: Props) {
 			<div>
 				<Image src={props.cartProduct.thumbnail} alt={props.cartProduct.title} width={300} height={300} className={'object-contain max-w-full'} />
 			</div>
-			<div className={'flex flex-1 items-start justify-between'}>
-				<div className={'flex flex-col flex-1 gap-2'}>
-					<h2 className={'text-2xl font-semibold'}>{props.cartProduct.title}</h2>
-					<p>{props.cartProduct.description}</p>
-					<QuantitySelector quantity={props.cartProduct.quantity} onQuantityChange={direction => onQuantityChangeHandler(direction)} />
+			<div className={'flex flex-1 items-start justify-between gap-2'}>
+				<div className={'flex flex-col justify-between flex-1 gap-2 h-full'}>
+					<div>
+						<h2 className={'text-2xl font-semibold'}>{props.cartProduct.title}</h2>
+						<p>{props.cartProduct.description}</p>
+					</div>
+					<div className={'flex items-center justify-between'}>
+						<QuantitySelector quantity={props.cartProduct.quantity} onQuantityChange={direction => onQuantityChangeHandler(direction)} />
+					</div>
 				</div>
-				<div className={''}>
-					{props.cartProduct.quantity > 1 && <span>{props.cartProduct.quantity} x </span>}
-					<strong className={'text-2xl font-medium'}>{toCurrency(props.cartProduct.price)}</strong>
+				<div className={'flex flex-col justify-between h-full min-w-48'}>
+					<div>
+						{props.cartProduct.quantity > 1 && <span>{props.cartProduct.quantity} x </span>}
+						<strong className={'text-2xl font-medium'}>{toCurrency(props.cartProduct.price)}</strong>
+					</div>
+					<Button buttonText={'rimuovi'} style={'danger'} onClick={() => removeProductFromCart(props.cartProduct.id)} />
 				</div>
 			</div>
 		</div>
